0de3c808cc9d81f25090169b24c974cb355e1dd7
Hugo Lemieux <h.lemieux66@gmail.com>
2024-05-28 20:25:41 -0400

    header done

---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+
+function Header () {
+  return (
+    <header className="header-container">
+      <h5 className="header-container__title">Hugo Lemieux</h5>
+      <nav className="header-container__nav">
+        <a className="header-container__nav-link">Expertise</a>
+        <a className="header-container__nav-link">À propos</a>
+        <a className="header-container__nav-link">Contact</a>
+      </nav>
+    </header>
+  )
+
+}
+
+export default Header
